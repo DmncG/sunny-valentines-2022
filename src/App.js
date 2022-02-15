d18b7cf81e11431c296a0cfa9637f9b89b53350a
DmncG <micksdev@gmail.com>
2022-02-14 21:58:44 -0500

    added image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import beach from './beach.png';
 
 function App() {
   return (
     <div className="App">
       <div className="blender__div">
-        <img className="blender-image" alt="" />
+        <img className="blender__image" alt="beach" src={beach} />
       </div>
 
       <div className="message__div">
@@ -12,10 +13,10 @@ function App() {
           Happy Valentines Day, 써니!
         </h1>
         <p className="content__p">
-          I know you'd rather get letters than receive flowers so instead of sending you a postcard or letter, I made a website instead (I'm playing to my strengths lol).
+          I know you'd rather get letters than receive flowers so instead of sending you a postcard or a traditional letter, I made a website instead (I'm playing to my strengths lol).
         </p>
         <p className="content__p">
-        I hope you're doing well in Las Vegas. I'm sure you're still having so much fun despite how busy you are. I hope you're still icing and taping that ankle. I'm still worried that I made you walk too much during your visit.
+        I hope you're doing well in Las Vegas. I'm sure you're still having so much fun despite how busy you are. Are you still icing and taping your ankle? I'm still worried that I made you walk too much during your visit.
         </p>
         <p className="content__p">
         Only a month or so before we go on a date again! Can't wait to go to the beach with you. I miss you!
@@ -24,7 +25,7 @@ function App() {
           Dom
         </p>
         <p className="content__p">
-          P.S. I was tempted to write all this in Korean but Google translate is too formal therefore, not cool lol. See you soon!
+          P.S. I was tempted to write all this in Korean but Google Translate is too formal. Therefore, not cool lol. See you soon!
         </p>
       </div>
     </div>
