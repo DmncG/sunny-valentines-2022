52608c36c6b79e9501d9968338d8b2a3b3025f01
DmncG <micksdev@gmail.com>
2022-02-14 21:37:29 -0500

    added message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="blender__div">
+        <img className="blender-image" alt="" />
+      </div>
+
+      <div className="message__div">
+        <h1 className="header__h1">
+          Happy Valentines Day, 써니!
+        </h1>
+        <p className="content__p">
+          I know you'd rather get letters than receive flowers so instead of sending you a postcard or letter, I made a website instead (I'm playing to my strengths lol).
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="content__p">
+        I hope you're doing well in Las Vegas. I'm sure you're still having so much fun despite how busy you are. I hope you're still icing and taping that ankle. I'm still worried that I made you walk too much during your visit.
+        </p>
+        <p className="content__p">
+        Only a month or so before we go on a date again! Can't wait to go to the beach with you. I miss you!
+        </p>
+        <p className="content__p">
+          Dom
+        </p>
+        <p className="content__p">
+          P.S. I was tempted to write all this in Korean but Google translate is too formal therefore, not cool lol. See you soon!
+        </p>
+      </div>
     </div>
   );
 }
